132743e1ab864113e719cc8c125556e28c497657
okaka favour <okakafavour81@gmail.com>
2026-05-28 03:59:12 +0100

    fix render env loading

--- config/database.go
+++ config/database.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -17,15 +16,11 @@ func ConnectDatabase() {
 	// Load .env only for local development
 	_ = godotenv.Load()
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_SSLMODE"),
-	)
+	dsn := os.Getenv("DATABASE_URL")
+
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -34,5 +29,5 @@ func ConnectDatabase() {
 
 	DB = database
 
-	fmt.Println("Database Connected Successfully")
+	log.Println("Database Connected Successfully")
 }
