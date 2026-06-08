f007392e979aa1075690cd96cafba1c1ce66272d
okaka favour <okakafavour81@gmail.com>
2026-06-08 04:55:19 +0100

    new feature added

--- internal/models/user.go
+++ internal/models/user.go
@@ -6,6 +6,6 @@ type User struct {
 	gorm.Model
 	Name     string `json:"name"`
 	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"_"`
+	Password string `json:"-"`
 	Role     string `json:"role"` // admin, cashier, pharmacist, manager
 }
